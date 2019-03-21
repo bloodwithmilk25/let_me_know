1b5c4f450737e30aae940d0d665cd75d7a1b0d85
bloodwithmilk <humapen@gmail.com>
2019-03-21 23:10:40 +0200

    update

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,5 @@
 from django.urls import re_path, include
 from .views import CustomConfirmEmailView
-from rest_auth.registration.views import (
-    SocialAccountListView, SocialAccountDisconnectView
-)
 
 urlpatterns = [
     re_path(r'^registration/account-confirm-email/(?P<key>[-:\w]+)/$', CustomConfirmEmailView.as_view(),
@@ -10,17 +7,4 @@ urlpatterns = [
     re_path(r'^registration/', include('rest_auth.registration.urls')),
     re_path(r'^accounts/', include('allauth.urls')),
     re_path(r'^', include('rest_auth.urls')),
-]
-
-urlpatterns += [
-    re_path(
-        r'^socialaccounts/$',
-        SocialAccountListView.as_view(),
-        name='social_account_list'
-    ),
-    re_path(
-        r'^socialaccounts/(?P<pk>\d+)/disconnect/$',
-        SocialAccountDisconnectView.as_view(),
-        name='social_account_disconnect'
-    )
 ]
