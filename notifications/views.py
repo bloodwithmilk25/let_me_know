42d9ab33c2dd7a1fe216cebbaa035ae1dc586b60
Nikita <humapen@gmail.com>
2019-03-26 15:35:22 +0200

    'upd'

--- notifications/views.py
+++ notifications/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 
 from .models import Notification
 from .serializers import NotificationSerializer
+from .permissions import IsOwner
 
 
 class NotificationsListView(generics.ListAPIView):
@@ -32,59 +33,25 @@ class NotificationDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
     GET Notifications/:id/
     PUT Notifications/:id/
+    PATCH Notifications/:id/
     DELETE Notifications/:id/
     """
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
-    # TODO user can only see/edit/delete his notifications
-    # TODO user can only create notifications for himself
-    # TODO patch
-    # TODO post
+    permission_classes = (IsOwner,)
 
-    def get(self, request, *args, **kwargs):
-        try:
-            notification = self.queryset.get(pk=kwargs["pk"])
-            if self.request.user.id == notification.user_id:
-                return Response(NotificationSerializer(notification).data)
-            return Response(
-                data={
-                    "message": "Your have no access this data"
-                },
-                status=status.HTTP_403_FORBIDDEN
-            )
+    def put_patch(self, request, *args, **kwargs):
+        serializer = NotificationSerializer(data=request.data)
+        if serializer.is_valid():
+            # check if user creating notification for himself  # maybe deprecated later
+            new_ntfc_user_id = serializer.validated_data['user'].id
+            if new_ntfc_user_id == self.request.user.id:
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        except Notification.DoesNotExist:
-            return Response(
-                data={
-                    "message": "Notification with id: {} does not exist".format(kwargs["pk"])
-                },
-                status=status.HTTP_404_NOT_FOUND
-            )
+    def patch(self, request, *args, **kwargs):
+        return self.put_patch(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        try:
-            notification = self.queryset.get(pk=kwargs["pk"])
-            serializer = NotificationSerializer()
-            updated_Notification = serializer.update(notification, request.data)
-            return Response(NotificationSerializer(updated_Notification).data)
-        except Notification.DoesNotExist:
-            return Response(
-                data={
-                    "message": "Notification with id: {} does not exist".format(kwargs["pk"])
-                },
-                status=status.HTTP_404_NOT_FOUND
-            )
-
-    def delete(self, request, *args, **kwargs):
-        try:
-            notification = self.queryset.get(pk=kwargs["pk"])
-            notification.delete()
-            return Response(data={"message": "Notification with id: {} was deleted".format(kwargs["pk"])},
-                            status=status.HTTP_204_NO_CONTENT)
-        except Notification.DoesNotExist:
-            return Response(
-                data={
-                    "message": "Notification with id: {} does not exist".format(kwargs["pk"])
-                },
-                status=status.HTTP_404_NOT_FOUND
-            )
+    def p(self, request, *args, **kwargs):
+        return self.put_patch(request, *args, **kwargs)
