8579b9b93e214b80e9a9198b7d2e81ec3e3896d5
bloodwithmilk <humapen@gmail.com>
2019-05-05 21:35:20 +0300

    upd

--- letmeknow/settings.py
+++ letmeknow/settings.py
@@ -116,7 +116,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
-        # 'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('POSTGRES_HOST'),
         'PORT': os.environ.get('POSTGRES_PORT')
     }
